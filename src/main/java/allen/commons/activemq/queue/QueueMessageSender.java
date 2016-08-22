c7da2470501b5827d08ce06c8124f66f1b3ba94f
allen <allen>
2016-08-22 23:20:44 +0800

    提交测试 代码

--- src/main/java/allen/commons/activemq/queue/QueueMessageSender.java
+++ src/main/java/allen/commons/activemq/queue/QueueMessageSender.java
@@ -1,5 +1,19 @@
 package allen.commons.activemq.queue;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import allen.commons.activemq.base.MQBaseData;
+
 
 /**
  * @author allen
@@ -7,11 +21,63 @@ package allen.commons.activemq.queue;
  * @date 2016年8月22日 下午10:37:13
  *
  */
-public class QueueMessageSender {
+public class QueueMessageSender extends MQBaseData{
 	
 	
 	//消息主题
-	private final static String DESTITION = "";
+	private final static String DESTINATION = "mq.queue.test.destination";
+	
+	
+	
+	
+	public static void run() throws Exception{
+		QueueConnectionFactory factory = new ActiveMQConnectionFactory(ActiveMQConnectionFactory.DEFAULT_USER,ActiveMQConnectionFactory.DEFAULT_PASSWORD,ACTIVE_MQ_BROKER_URL);
+		
+		QueueConnection connection = factory.createQueueConnection();
+		
+		connection.start();
+		
+		QueueSession session = connection.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
+		
+		Queue destination = session.createQueue(DESTINATION);
+		
+		QueueSender sender = session.createSender(destination);
+		
+		sendMessage(sender, session);
+		
+		session.commit();
+		
+		
+		if(session != null)  session.close();
+		
+		if(connection != null) connection.close();
+	}
+	
+	
+	
+	public static void sendMessage(QueueSender sender, Session session) throws Exception{
+		
+		for (int i = 0; i < SEND_NUM; i++) { 
+			
+			String text = "发送第:" + (i+1) + "条消息";
+			MapMessage message = session.createMapMessage();
+			message.setString("text",text);
+			message.setLong("time", System.currentTimeMillis());
+			
+			System.out.println(message.toString());
+			
+			sender.send(message);
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	/**
@@ -19,8 +85,8 @@ public class QueueMessageSender {
 	 * @param
 	 * @return
 	 */
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		run();
 	}
 
 }
