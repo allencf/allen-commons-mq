aea2b37c234f0adaa02006b65ef631bbe027c96b
allen <allen>
2016-09-05 23:10:29 +0800

    添加测试代码

--- src/test/java/allen/commons/activemq/test/AppTest.java
+++ src/test/java/allen/commons/activemq/test/AppTest.java
@@ -1,5 +1,26 @@
 package allen.commons.activemq.test;
 
 public class AppTest {
+	
+	
+	public static void main(String[] args) {
+		String s1 = "疯狂JAVA";
+		String s2 = "疯狂";
+		String s3 = "JAVA";
+		
+		String s4 = "疯狂" + "JAVA";
+		String s5 = "疯" + "狂" + "JAVA" ;
+		String s6 = s2 + s3;
+		
+		String s7 = new String("疯狂JAVA");
+		
+		System.out.println(s1==s4);
+		
+		System.out.println(s1 == s5);
+		
+		System.out.println(s1== s6);
+		
+		System.out.println(s1 == s7);
+	}
 
 }
